bce7826ac1ab1508284b97c7e00ec581495595cb
Jibendu Paul <bbiswas.paul@gmail.com>
2021-07-22 21:58:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Movie from './components/Movie';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const  movies = ['1' , '2' ,  '3'];
+  return <div>
+    {movies.map(movie =>(
+      <Movie />
+    ))}
+    
+    </div>;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Movie = () => (
+    <div className ="movie">
+        This is a Movie Component
+    </div>
+);
+export default Movie;
